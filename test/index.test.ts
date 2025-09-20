639c205e3399ecdf71eb4cd30be787c7be683614
Ed Dorsey <ed@polarity.io>
2025-09-20 08:37:47 -0400

    update tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,15 +3,11 @@ import { helpers, userOptions } from '../lib/index';
 import { parseErrorToReadableJson } from '../lib/errors';
 
 const {
-  decodeBase64,
-  encodeBase64,
   filterObjectsContainingString,
   mapOverObject,
   transpose2dArray,
   and,
-  or,
-  millisecondsToHoursMinutesAndSeconds,
-  sleep
+  or
 } = helpers;
 
 const { validateUrlOption } = userOptions;
@@ -20,15 +16,11 @@ import { setLogger, getLogger } from '../lib/logging';
 describe('index.js', () => {
   it('should export functions with implementation', () => {
     expect(parseErrorToReadableJson).toBeInstanceOf(Function);
-    expect(decodeBase64).toBeInstanceOf(Function);
-    expect(encodeBase64).toBeInstanceOf(Function);
     expect(filterObjectsContainingString).toBeInstanceOf(Function);
     expect(mapOverObject).toBeInstanceOf(Function);
     expect(transpose2dArray).toBeInstanceOf(Function);
     expect(and).toBeInstanceOf(Function);
     expect(or).toBeInstanceOf(Function);
-    expect(millisecondsToHoursMinutesAndSeconds).toBeInstanceOf(Function);
-    expect(sleep).toBeInstanceOf(Function);
     expect(validateUrlOption).toBeInstanceOf(Function);
     expect(setLogger).toBeInstanceOf(Function);
     expect(getLogger).toBeInstanceOf(Function);
