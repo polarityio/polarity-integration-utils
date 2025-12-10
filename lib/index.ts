42c8356a9fa1343d8a1232c40bc5a97b36d0cbbb
Collin Rogers <collin@polarity.io>
2025-12-10 12:42:42 -0500

    feat(schemas): add zod schemas and test helpers

--- lib/index.ts
+++ lib/index.ts
@@ -2,5 +2,8 @@ import * as errors from './errors';
 import * as requests from './requests';
 import * as logging from './logging';
 import * as context from './context';
+import * as types from './types';
 
-export { errors, requests, logging, context };
+export * from './types';
+
+export { errors, requests, logging, context, types };
---
+++ lib/testing/index.ts
@@ -0,0 +1,5 @@
+export * from './test-factory/create-integration-tests';
+export * from './enhanced-utils/create-entity';
+export * from './enhanced-utils/create-mock-integration-context';
+export * from './enhanced-utils/mock-request';
+export * from './enhanced-utils/validate-integration';
